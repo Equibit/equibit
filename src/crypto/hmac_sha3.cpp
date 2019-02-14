2aa008540a97b8f0c12b633db33ef6ac39b7d2e7
David McFadzean <davidmc@gmail.com>
2019-02-14 16:59:25 -0500

    Fixed block size in HMAC-SHA3

--- src/crypto/hmac_sha3.cpp
+++ src/crypto/hmac_sha3.cpp
@@ -8,22 +8,23 @@
 
 CHMAC_SHA3::CHMAC_SHA3(const unsigned char* key, size_t keylen)
 {
-    unsigned char rkey[64];
-    if (keylen <= 64) {
+    // Block size of 136 from Table 3 FIPS 202 http://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.202.pdf
+    unsigned char rkey[136];
+    if (keylen <= 136) {
         memcpy(rkey, key, keylen);
-        memset(rkey + keylen, 0, 64 - keylen);
+        memset(rkey + keylen, 0, 136 - keylen);
     } else {
         CSHA3().Write(key, keylen).Finalize(rkey);
         memset(rkey + 32, 0, 32);
     }
 
-    for (int n = 0; n < 64; n++)
+    for (int n = 0; n < 136; n++)
         rkey[n] ^= 0x5c;
-    outer.Write(rkey, 64);
+    outer.Write(rkey, 136);
 
-    for (int n = 0; n < 64; n++)
+    for (int n = 0; n < 136; n++)
         rkey[n] ^= 0x5c ^ 0x36;
-    inner.Write(rkey, 64);
+    inner.Write(rkey, 136);
 }
 
 void CHMAC_SHA3::Finalize(unsigned char hash[OUTPUT_SIZE])
