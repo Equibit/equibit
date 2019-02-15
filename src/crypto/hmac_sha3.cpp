2af0d2262f09b65f75c96371de1b24697efb0ce2
David McFadzean <davidmc@gmail.com>
2019-02-15 10:52:35 -0500

    Refactored (DRY principle)
    http://wiki.c2.com/?DontRepeatYourself

--- src/crypto/hmac_sha3.cpp
+++ src/crypto/hmac_sha3.cpp
@@ -1,4 +1,5 @@
 // Copyright (c) 2014-2017 The Bitcoin Core developers
+// Copyright (c) 2019 The Equibit Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
@@ -9,22 +10,24 @@
 CHMAC_SHA3::CHMAC_SHA3(const unsigned char* key, size_t keylen)
 {
     // Block size of 136 from Table 3 FIPS 202 http://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.202.pdf
-    unsigned char rkey[136];
-    if (keylen <= 136) {
+    const unsigned int blockSize = 136;
+
+    unsigned char rkey[blockSize];
+    if (keylen <= blockSize) {
         memcpy(rkey, key, keylen);
-        memset(rkey + keylen, 0, 136 - keylen);
+        memset(rkey + keylen, 0, blockSize - keylen);
     } else {
         CSHA3().Write(key, keylen).Finalize(rkey);
         memset(rkey + 32, 0, 32);
     }
 
-    for (int n = 0; n < 136; n++)
+    for (int n = 0; n < blockSize; n++)
         rkey[n] ^= 0x5c;
-    outer.Write(rkey, 136);
+    outer.Write(rkey, blockSize);
 
-    for (int n = 0; n < 136; n++)
+    for (int n = 0; n < blockSize; n++)
         rkey[n] ^= 0x5c ^ 0x36;
-    inner.Write(rkey, 136);
+    inner.Write(rkey, blockSize);
 }
 
 void CHMAC_SHA3::Finalize(unsigned char hash[OUTPUT_SIZE])
